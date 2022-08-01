9e2cf808eeb871b6fc622e8e1909025f1d0196d2
joelsouza95 <53006969+joelsouza95@users.noreply.github.com>
2022-08-01 13:08:00 -0400

    Slideshow

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$('.Slick-Principal').slick({
+      dots: true,
+      infinite: true,
+      speed: 800,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+  });
